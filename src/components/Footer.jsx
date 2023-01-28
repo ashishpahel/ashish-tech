a3d25400336033aea50bf23f073745a9a793b8cd
ashishpahel <ashishpahel1999@gmail.com>
2023-01-28 23:34:39 +0530

    first-commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+       <div>
+        <h1>Ashish Pahel</h1>
+        <p>@all right reserved by AshishPahel</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/ashishpahel/" target={"blank"}>
+            Linkedin
+          </a>
+          
+          <a href="https://www.instagram.com/ashishpahel/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/ashishpahel" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
